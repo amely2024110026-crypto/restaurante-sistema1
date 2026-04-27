f9402abae4302ad80cdf06d3ae618377dbe53bb3
AMELY PAOLA CARRETO PEREZ <amely.2024110026@metropoli.edu.mx>
2026-04-27 07:46:43 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<?php include("auth.php"); ?>
-<?php include("menu.php"); ?>
-<?php include("config.php"); ?>
-
-<link rel="stylesheet" href="<?php echo $base; ?>assets/css/styles.css">
-
-<div class="main">
-
-<div class="nav">
-<span>👋 <?php echo $_SESSION['usuario']; ?></span>
-<span><?php echo $_SESSION['rol']; ?></span>
-</div>
-
-<div class="card">
-<h2>Panel Principal</h2>
-<p>Sistema funcionando correctamente</p>
-</div>
-
-</div>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=dashboard.html">
+  </head>
+  <body>
+    <p>Redirigiendo al panel...</p>
+  </body>
+</html>
